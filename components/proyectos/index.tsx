b8154989951cb8ca36247692b43da9e489541aab
brunoken22 <bruno_am_22@hotmail.com>
2023-07-30 18:19:42 -0300

    vc

---
+++ components/proyectos/index.tsx
@@ -0,0 +1,14 @@
+import { TemplateProyect } from "./styled"
+import { Subtitle } from "@/ui/typography"
+export function Proyectos(){
+   return(
+      <div>
+         <div>
+            <Subtitle>Experiencia</Subtitle>
+         </div>
+         <TemplateProyect>
+            
+         </TemplateProyect>
+      </div>
+   )
+}
