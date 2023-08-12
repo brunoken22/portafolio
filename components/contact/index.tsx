316a44ed60f42c5d2ec315cbf27f75c10a6e9e27
Bruno Ken <bruno_am_22@hotmail.com>
2023-08-12 12:35:35 -0300

    mejorando1

--- components/contact/index.tsx
+++ components/contact/index.tsx
@@ -1,74 +1,120 @@
-import {Label,Input,Button,Form,ContenedorForm,Textarea,DivContact,OneContact,Enviado, Span, Enlaces}from './styled'
-import { Body,Subtitle } from '@/ui/typography'
-import { useForm } from "react-hook-form";
-import Linkedin from '@/ui/icons/linkedin.svg'
-import Github from '@/ui/icons/github.svg'
+import {
+  Label,
+  Input,
+  Button,
+  Form,
+  ContenedorForm,
+  Textarea,
+  DivContact,
+  OneContact,
+  Enviado,
+  Span,
+  Enlaces,
+} from './styled';
+import {Body, Subtitle} from '@/ui/typography';
+import {useForm} from 'react-hook-form';
+import Linkedin from '@/ui/icons/linkedin.svg';
+import Github from '@/ui/icons/github.svg';
 import Link from 'next/link';
-import {  Slide } from "react-awesome-reveal";
-import { mensaje } from '@/lib/hook';
+import {Slide} from 'react-awesome-reveal';
+import {mensaje} from '@/lib/hook';
 type FormData = {
-   name: string;
-   email: string;
-   message:string;
+  name: string;
+  email: string;
+  message: string;
 };
-export function Contact(){
-   const { register, setValue, handleSubmit, formState,reset } = useForm<FormData>();
+export function Contact() {
+  const {register, setValue, handleSubmit, formState, reset} =
+    useForm<FormData>();
 
-   const onSubmit = async(e:any) => {
-      await mensaje(      {
-         name:e.name,
-         email:e.email,
-         message:e.message
-     })
-     setValue("name","")
-     setValue("email","")
-     setValue("message","")
+  const onSubmit = async (e: any) => {
+    await mensaje({
+      name: e.name,
+      email: e.email,
+      message: e.message,
+    });
+    setValue('name', '');
+    setValue('email', '');
+    setValue('message', '');
 
-      alert("mensaje enviado")
-   };
- 
+    alert('mensaje enviado');
+  };
 
-   return (
-      <ContenedorForm id="2">
-         <Slide>
-         <DivContact>
-            <div>
-               <Body><Span >Datos de contacto</Span></Body>
-               <Subtitle>¡Trabajemos juntos!</Subtitle>
+  return (
+    <ContenedorForm id='2'>
+      <Slide triggerOnce>
+        <DivContact>
+          <div>
+            <Body>
+              <Span>Datos de contacto</Span>
+            </Body>
+            <Subtitle>¡Trabajemos juntos!</Subtitle>
+          </div>
+          <OneContact>
+            <div style={{display: 'flex', alignItems: 'center'}}>
+              <Body>Tlf: </Body>
+              <Enlaces href='tel:+541161204047'> +541161204047</Enlaces>
             </div>
-            <OneContact>
-               <div style={{display: "flex",alignItems:"center"}}><Body>Tlf: </Body><Enlaces href="tel:+541161204047" > +541161204047</Enlaces></div>
-               <div style={{display: "flex",alignItems:"center"}}><Body>Email: </Body><Enlaces href="mailto:bruno_am_22@hotmail.com"> bruno_am_22@hotmail.com</Enlaces></div>
-            </OneContact>
-            <OneContact $direction="row">
-               <Link href="https://github.com/brunoken22" aria-label="github" target="_blank"><Github/></Link>
-               <Link href="https://www.linkedin.com/in/brunoken18/" aria-label="linkedin" target="_blank"><Linkedin/></Link>   
-            </OneContact>
-         </DivContact>
-         <Form onSubmit={handleSubmit(onSubmit)}>
-            <div>
-               <Label htmlFor='name'>Nombre</Label>
-               <Input type='text' {...register("name")} id="name" placeholder='Bruno Ken'></Input>
+            <div style={{display: 'flex', alignItems: 'center'}}>
+              <Body>Email: </Body>
+              <Enlaces href='mailto:bruno_am_22@hotmail.com'>
+                {' '}
+                bruno_am_22@hotmail.com
+              </Enlaces>
             </div>
-            <div>
-               <Label htmlFor='email'>Email</Label>
-               <Input type='email' {...register("email")} id="email" placeholder='Bruno_am_22@hotmail.com'></Input>
-            </div>
-            <div>
-               <Label htmlFor='message'>Mensaje</Label>
-               <Textarea  {...register("message")} id="message"></Textarea>
-            </div>
-            <div style={{display:"flex",alignItems:"center",justifyContent:"center"}}>
-               <Button>Enviar</Button>
-            </div>
+          </OneContact>
+          <OneContact $direction='row'>
+            <Link
+              href='https://github.com/brunoken22'
+              aria-label='github'
+              target='_blank'>
+              <Github />
+            </Link>
+            <Link
+              href='https://www.linkedin.com/in/brunoken18/'
+              aria-label='linkedin'
+              target='_blank'>
+              <Linkedin />
+            </Link>
+          </OneContact>
+        </DivContact>
+        <Form onSubmit={handleSubmit(onSubmit)}>
+          <div>
+            <Label htmlFor='name'>Nombre</Label>
+            <Input
+              type='text'
+              {...register('name')}
+              id='name'
+              placeholder='Bruno Ken'></Input>
+          </div>
+          <div>
+            <Label htmlFor='email'>Email</Label>
+            <Input
+              type='email'
+              {...register('email')}
+              id='email'
+              placeholder='Bruno_am_22@hotmail.com'></Input>
+          </div>
+          <div>
+            <Label htmlFor='message'>Mensaje</Label>
+            <Textarea {...register('message')} id='message'></Textarea>
+          </div>
+          <div
+            style={{
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'center',
+            }}>
+            <Button>Enviar</Button>
+          </div>
 
-            {formState.isSubmitted && (
-               <div>
-                  <Enviado>Formulario enviado, Revisa tu correo</Enviado>
-               </div>
-            )}
-         </Form>
-         </Slide>
-      </ContenedorForm>
-   )
+          {formState.isSubmitted && (
+            <div>
+              <Enviado>Formulario enviado, Revisa tu correo</Enviado>
+            </div>
+          )}
+        </Form>
+      </Slide>
+    </ContenedorForm>
+  );
 }
--- components/proyectos/index.tsx
+++ components/proyectos/index.tsx
@@ -1,109 +1,137 @@
-"use client"
-import * as contentful from 'contentful'
-import { TemplateProyect,DivProyectos,Botton } from "./styled"
-import { Subtitle,Body } from "@/ui/typography"
-import Image from 'next/image'
-import { useEffect, useState } from 'react'
-import Link from 'next/link'
-import Heart from '@/ui/icons/heart.svg'
-import {Zoom} from "react-awesome-reveal";
-import { ObtenerLikes,SubirLikes,QuitarLike } from '@/lib/hook'
+'use client';
+import * as contentful from 'contentful';
+import {TemplateProyect, DivProyectos, Botton} from './styled';
+import {Subtitle, Body} from '@/ui/typography';
+import Image from 'next/image';
+import {useEffect, useState} from 'react';
+import Link from 'next/link';
+import Heart from '@/ui/icons/heart.svg';
+import {Zoom} from 'react-awesome-reveal';
+import {ObtenerLikes, SubirLikes, QuitarLike} from '@/lib/hook';
 
 const client = contentful.createClient({
   space: 'dehbm7ub5p2i',
   accessToken: 'vfQ16mfGkwZCKSPTKJypjjLoKDZH_6mZgkD__9if1ws',
-})
-export function Proyectos(){
-   const [proyect,setProyect]  = useState()
-   const { data, error, isLoading } = ObtenerLikes();
+});
+export function Proyectos() {
+  const [proyect, setProyect] = useState();
+  const {data, error, isLoading} = ObtenerLikes();
 
+  useEffect(() => {
+    client.getEntries({content_type: 'cms'}).then((response: any) => {
+      const productos = response.items;
+      setProyect(productos);
+    });
+  }, []);
 
-   useEffect(()=>{
-      client.getEntries({ content_type: 'cms' })
-      .then((response:any) => {
-         const productos = response.items;
-         setProyect(productos)
-      })
-   },[])
-
-
-   return(
-      <div id="1">
-         <div>
-            <h2 style={{textAlign:"center"}}>Experiencia</h2>
-         </div>
-         <DivProyectos>
-            {proyect && data?(proyect as any).map((el:any,p:number)=>{return (<div key={p}><Zoom><TemplateProyect ><Link id={data[p].id} href={el.fields.linkDeArticle} key={p} target='blank'>
-               <Image src={el.fields.linkImgPagina} width={350} height={230} alt={el.fields.appMisPelis}></Image></Link>
-               <div >
-                  <Body>{el.fields.appMisPelis}</Body>
-                  <Like data={data[p]}/>
-               </div>
-            </TemplateProyect></Zoom></div>)}):null}
-         </DivProyectos>
-      </div>   
-   )
+  return (
+    <div id='1'>
+      <div>
+        <h2 style={{textAlign: 'center'}}>Experiencia</h2>
+      </div>
+      <DivProyectos>
+        {proyect && data
+          ? (proyect as any).map((el: any, p: number) => {
+              return (
+                <div key={p}>
+                  <Zoom triggerOnce>
+                    <TemplateProyect>
+                      <Link
+                        id={data[p].id}
+                        href={el.fields.linkDeArticle}
+                        key={p}
+                        target='blank'>
+                        <Image
+                          src={el.fields.linkImgPagina}
+                          width={350}
+                          height={230}
+                          alt={el.fields.appMisPelis}></Image>
+                      </Link>
+                      <div>
+                        <Body>{el.fields.appMisPelis}</Body>
+                        <Like data={data[p]} />
+                      </div>
+                    </TemplateProyect>
+                  </Zoom>
+                </div>
+              );
+            })
+          : null}
+      </DivProyectos>
+    </div>
+  );
 }
 
- function Like (props:any){
-   const [contador,setContador] = useState(props.data.likes)
-   const [newData,setNewData] = useState({id:"",proyect:"",like:0})
-   const [modData,setModData] = useState({id:"",proyect:"",like:0})
+function Like(props: any) {
+  const [contador, setContador] = useState(props.data.likes);
+  const [newData, setNewData] = useState({id: '', proyect: '', like: 0});
+  const [modData, setModData] = useState({id: '', proyect: '', like: 0});
 
-   useEffect(()=>{
-      setContador(props.data.likes)
-   },[props.data.likes])
+  useEffect(() => {
+    setContador(props.data.likes);
+  }, [props.data.likes]);
 
-   const handleClick = async(e:any)=>{
-      e.preventDefault();
-      e.target.style.disabled = true
-      e.target.style.cursor = "progress";
-      await SubirLikes({
-         id:props.data.id,
-         proyect:props.data.proyect,
-         like:contador+1
-      });
-      e.target.style.disabled = false
-      e.target.style.cursor = "pointer";
-      e.target.style.fill = "tomato"
-      
-      setContador(((e:number)=>e+1))
-      setNewData({
-         id:props.data.id,
-         proyect:props.data.proyect,
-         like:contador+1
-      })
-      setModData({
-         id:"",
-         proyect:"",
-         like:0
-      })
-   }
+  const handleClick = async (e: any) => {
+    e.preventDefault();
+    e.target.style.disabled = true;
+    e.target.style.cursor = 'progress';
+    await SubirLikes({
+      id: props.data.id,
+      proyect: props.data.proyect,
+      like: contador + 1,
+    });
+    e.target.style.disabled = false;
+    e.target.style.cursor = 'pointer';
+    e.target.style.fill = 'tomato';
 
-   const handleDobleClick = async(e:any)=>{
-      e.preventDefault();
-      e.target.style.disabled = true
-      e.target.style.cursor = "progress";
-      await QuitarLike({
-         id:props.data.id,
-         proyect:props.data.proyect,
-         like:contador-1
-      })
-      e.target.style.disabled = false
-      e.target.style.cursor = "pointer";
-      e.target.style.fill = "#ddd"  
-      setContador(((e:number)=>e-1))
-      setNewData({
-         id:"",
-         proyect:"",
-         like:0
-      })
-   }
+    setContador((e: number) => e + 1);
+    setNewData({
+      id: props.data.id,
+      proyect: props.data.proyect,
+      like: contador + 1,
+    });
+    setModData({
+      id: '',
+      proyect: '',
+      like: 0,
+    });
+  };
 
-   return (
-      <div style={{display: "flex",justifyContent: "space-between",alignItems: "center",padding: "1rem",height:"90px"}} >
-         <Body $weight="100" $size={"1rem"}>{contador} Me gusta</Body>
-         <Botton onClick={newData.id?handleDobleClick:handleClick}><Heart ></Heart></Botton>
-      </div>
-   )
+  const handleDobleClick = async (e: any) => {
+    e.preventDefault();
+    e.target.style.disabled = true;
+    e.target.style.cursor = 'progress';
+    await QuitarLike({
+      id: props.data.id,
+      proyect: props.data.proyect,
+      like: contador - 1,
+    });
+    e.target.style.disabled = false;
+    e.target.style.cursor = 'pointer';
+    e.target.style.fill = '#ddd';
+    setContador((e: number) => e - 1);
+    setNewData({
+      id: '',
+      proyect: '',
+      like: 0,
+    });
+  };
+
+  return (
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        padding: '1rem',
+        height: '90px',
+      }}>
+      <Body $weight='100' $size={'1rem'}>
+        {contador} Me gusta
+      </Body>
+      <Botton onClick={newData.id ? handleDobleClick : handleClick}>
+        <Heart></Heart>
+      </Botton>
+    </div>
+  );
 }
