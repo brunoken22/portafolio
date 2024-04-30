7a0c92f0f986f5de29846f9cdf11ad89c1b7658f
Bruno Ken <bruno_am_22@hotmail.com>
2024-04-30 12:05:39 -0300

    corrigiendo app

--- app/api/proyect/route.ts
+++ app/api/proyect/route.ts
@@ -1,15 +1,16 @@
 import {client} from '@/lib/contenful';
+import {NextResponse} from 'next/server';
 export async function GET() {
   try {
     let proyect;
     await client.getEntries({content_type: 'cms'}).then((response) => {
       proyect = response.items;
     });
-    return Response.json(proyect, {
+    return NextResponse.json(proyect, {
       status: 200,
     });
   } catch (e: any) {
-    return Response.json(e.message, {
+    return NextResponse.json(e.message, {
       status: 500,
     });
   }
