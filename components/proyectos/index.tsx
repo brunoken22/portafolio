9737c7659bc8a3443baba90ee260e969075fefc9
Bruno Ken <bruno_am_22@hotmail.com>
2024-08-03 18:33:38 -0300

    Mejorando app

--- components/proyectos/index.tsx
+++ components/proyectos/index.tsx
@@ -51,37 +51,39 @@ export default function Proyectos({proyect, data}: {proyect: any; data: any}) {
                   key={el.fields.id}
                   className='templateProyect'>
                   <TemplateProyect>
-                    <Link
-                      href={el.fields.linkDeArticle}
-                      target='blank'
-                      className='h-[230px] relative overflow-hidden [&>div]:hover:flex'>
+                    <div className='h-[230px] relative overflow-hidden [&>div]:hover:flex'>
                       <ImageProyect
                         src={el.fields.linkImgPagina}
                         loading='lazy'
                         alt={el.fields.appMisPelis}
                       />
                       <div className='absolute inset-0 backdrop-brightness-50  items-center justify-center hidden transition-all'>
-                        <svg
-                          xmlns='http://www.w3.org/2000/svg'
-                          width='50'
-                          height='50'
-                          viewBox='0 0 24 24'
-                          stroke-width='1.5'
-                          stroke='#00deff'
-                          fill='none'
-                          stroke-linecap='round'
-                          stroke-linejoin='round'>
-                          <path stroke='none' d='M0 0h24v24H0z' fill='none' />
-                          <path d='M21 12a9 9 0 1 0 -9 9' />
-                          <path d='M3.6 9h16.8' />
-                          <path d='M3.6 15h7.9' />
-                          <path d='M11.5 3a17 17 0 0 0 0 18' />
-                          <path d='M12.5 3a16.984 16.984 0 0 1 2.574 8.62' />
-                          <path d='M18 18m-3 0a3 3 0 1 0 6 0a3 3 0 1 0 -6 0' />
-                          <path d='M20.2 20.2l1.8 1.8' />
-                        </svg>
+                        <Link
+                          className='hover:opacity-70'
+                          href={el.fields.linkDeArticle}
+                          target='blank'>
+                          <svg
+                            xmlns='http://www.w3.org/2000/svg'
+                            width='50'
+                            height='50'
+                            viewBox='0 0 24 24'
+                            stroke-width='1.5'
+                            stroke='#00deff'
+                            fill='none'
+                            stroke-linecap='round'
+                            stroke-linejoin='round'>
+                            <path stroke='none' d='M0 0h24v24H0z' fill='none' />
+                            <path d='M21 12a9 9 0 1 0 -9 9' />
+                            <path d='M3.6 9h16.8' />
+                            <path d='M3.6 15h7.9' />
+                            <path d='M11.5 3a17 17 0 0 0 0 18' />
+                            <path d='M12.5 3a16.984 16.984 0 0 1 2.574 8.62' />
+                            <path d='M18 18m-3 0a3 3 0 1 0 6 0a3 3 0 1 0 -6 0' />
+                            <path d='M20.2 20.2l1.8 1.8' />
+                          </svg>
+                        </Link>
                       </div>
-                    </Link>
+                    </div>
                     <div className='grid grid-flow-col items-center overflow-y-hidden overflow-x-auto gap-4 pb-2 pl-2'>
                       {el.fields.technologiess.map(
                         (item: {title: string; svg: string}, t: number) => (
