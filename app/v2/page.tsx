887e911353d259ca2c4b5fd1d5193862dfc0e64d
brunoken22 <bruno_am_22@hotmail.com>
2025-09-07 16:35:58 -0300

    Actualizando .gitignore para incluir la carpeta de componentes V1 y eliminando el archivo de página de la versión 2; se simplifica la estructura del proyecto.

--- app/v2/page.tsx
+++
@@ -1,9 +0,0 @@
-import { fetchApiAuth } from "@/lib/hook";
-import V1 from "@/components/V1";
-
-export default async function Home() {
-  const proyect = await fetchApiAuth(["/api/proyect", {}]);
-  const data = await fetchApiAuth(["/api/like", {}]);
-
-  return <V1 proyect={proyect} data={data} />;
-}
